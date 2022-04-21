6e2afa064ea7b62a0f3f78ecf75c6233b013c4ee
YuliaTerzieva <yulia.terzieva@gmail.com>
2022-04-21 18:59:33 +0200

    Obtained two psychometric curves on data after CW and CCW

--- initial_data_analysis.py
+++ initial_data_analysis.py
@@ -1,14 +1,14 @@
-import math
-
+from math import log
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 from scipy.stats import norm
 from scipy.optimize import minimize
 
-def negLogL(params, rod_orients, frame, df):
-    """ This function is a helper one used by scipy.optimize.minimize
-    in order for a psychometric curve fitting
+
+def negLogL(params, rod_orients, frame, GivenData, comments):
+    """ This function is a helper one used by scipy.optimize.minimize for a psychometric curve fitting
+    :param GivenData:
     :param params: This is array of two values mu and sigma that we want to optimize
     :param rod_orients: array of possible rod orientations
     :param frame: single number holding the frame orientation of interest
@@ -16,60 +16,100 @@ def negLogL(params, rod_orients, frame, df):
     """
     mu = params[0]
     sigma = params[1]
+    if comments:
+        print(f" mu = {mu} and sigma = {sigma}")
     negLogLikelihood = 0
     for rod in rod_orients:
-        curr_df = df[(df['frameOri'] == frame) & (df['rodOri'] == rod)]
+        curr_df = GivenData[(GivenData['frameOri'] == frame) & (GivenData['rodOri'] == rod)]
         probs = norm.cdf(rod, mu, sigma)
-        # print(f"for rod {rod} we have probability of CW = {probs}")
+        if comments:
+            print(f"for rod {rod} we have probability of CW = {probs}")
         response = curr_df['response']
         response = response.tolist()
-        # print(f" the responses for this rod are {response}")
-        # print(f" the CW responses are {response.count(1)} and CCW {response.count(-1)}")
+        if comments:
+            print(f" the responses for this rod are {response}")
+            print(f" the CW responses are {response.count(1)} and CCW {response.count(-1)}")
         likelihood = response.count(1) * probs + response.count(-1) * (1 - probs)
-        # print(f" The likelihood is {likelihood}")
-        # print(f" The log likelihood is {math.log(likelihood)}")
-        negLogLikelihood += - math.log(likelihood)
+        if comments:
+            print(f" The likelihood is {likelihood}")
+            print(f" The log likelihood is {log(likelihood)}")
+        negLogLikelihood += - log(likelihood)
     return negLogLikelihood
 
 
+def createMatrixProb(dataframe):
+    # check the rod and frame orientations (rods depend on subject and condition)
+    rod_orients = np.sort(dataframe.rodOri.unique())
+    frame_orients = np.sort(dataframe.frameOri.unique())
+
+    # A matrix with rows the frame orientations and columns rod orientations
+    # where each frame/rod combination is the number of CW responses out of 10
+    probabilities = np.zeros((len(frame_orients), len(rod_orients)))
+
+    for f, frame in enumerate(frame_orients):
+        for r, rod in enumerate(rod_orients):
+            # Todone : 1) Now i need to make it such that the points are not out ot 10, but out of depending on the rod,
+            # because when i separate the data the rods won't be out of 10. -> I achieved this by first selecting only the
+            # data with frame and rod and the diving the CW responses by all the responses
+
+            # working_df are the rows in the dataset where we have the given frame and rod
+            working_df = dataframe[(dataframe['frameOri'] == frame) & (dataframe['rodOri'] == rod)]
+            if working_df.shape[0] > 0:
+                probabilities[f][r] = working_df[working_df['response'] == 1].shape[0] / working_df.shape[0]
+            else:
+                probabilities[f][r] = 0
+
+    return probabilities
+
+# THIS WORKS WELL FOR C2 AND C6 haven't check after that.
 # read in the data
 df = pd.read_csv('Controls/c2/c2_frame.txt', skiprows=13, sep=" ")
 # remove the last two columns (these are reactionTime and ??)
 df.drop('reactionTime', inplace=True, axis=1)
 df.drop('Unnamed: 4', inplace=True, axis=1)
-# check the rod and frame orientations (rods depend on subject and condition)
-rod_orients = np.sort(df.rodOri.unique())
-frame_orients = np.sort(df.frameOri.unique())
+rod_orients_all = np.sort(df.rodOri.unique())
+frame_orients_all = np.sort(df.frameOri.unique())
 
-# A matrix with rows the frame orientations and columns rod orientations
-# where each frame/rod combination is the number of CW responses out of 10
-probabilities = np.zeros((len(frame_orients), len(rod_orients)))
+# Separating the dataset based on CW and CCW responses on n-1
+CW_index = df.index[df['response'] == 1].tolist()
+CCW_index = df.index[df['response'] == -1].tolist()
 
-for f, frame in enumerate(frame_orients):
-    for r, rod in enumerate(rod_orients):
-        # Todone : 1) Now i need to make it such that the points are not out ot 10, but out of depending on the rod,
-        # because when i separate the data the rods won't be out of 10. -> I achieved this by first selecting only the
-        # data with frame and rod and the diving the CW responses by all the responses
+CW_next_index = [i + 1 for i in CW_index if i != df.shape[0] - 1]
+CCW_next_index = [i + 1 for i in CCW_index if i != df.shape[0] - 1]
 
-        # working_df are the rows in the dataset where we have the given frame and rod
-        working_df = df[(df['frameOri'] == frame) & (df['rodOri'] == rod)]
-        probabilities[f][r] = working_df[working_df['response'] == 1].shape[0] / working_df.shape[0]
 
-# Test for frame 0 degrees
-results = minimize(negLogL, [0, 2], args=(rod_orients, 0, df))
-print(results)
-print(f" The mu is {results.x[0]} and sigma is {results.x[0]}")
+CWdata = df.iloc[CW_next_index]
+CCWdata = df.iloc[CCW_next_index]
 
-plt.plot(rod_orients, probabilities[(np.where(frame_orients == 0)[0][0])], "o")
-plt.plot(rod_orients, norm.cdf(rod_orients, results.x[0], results.x[1]))
+# Obtaining statistics for n-1 = CW frame = 0
+# For the minimize in general I hough I should pass as a parameter for the rods np.(CWdata.rodOri.unique())
+# because it is possible that after separating the dataset there are some rod representations missing, but turns out
+# there is no such problem so for simplicity I used rod_orients_all
+
+# For both cases CW and CWW it is important that we put a constraint that sigma cannot be negative :
+cons = ({"type" : "ineq", "fun" : lambda params: params[1]})
+
+CW_post_probs = createMatrixProb(CWdata)
+print("***** START OF CW DATA ANALYSIS *****")
+CW_results = minimize(negLogL, [1, 1.5], args=(rod_orients_all, 0, CWdata, True), constraints=cons)
+print(f" Done with CW results {CW_results.x[0]} and {CW_results.x[1]}")
+
+# Obtaining statistics for n-1 = CCW frame = 0
+CCW_post_probs = createMatrixProb(CCWdata)
+print("***** START OF CCW DATA ANALYSIS *****")
+CCW_results = minimize(negLogL, [1, 1.5], args=(rod_orients_all, 0, CCWdata, False), constraints=cons)
+print(f" Done with CCW results {CCW_results.x[0]} and {CCW_results.x[1]}")
+
+
+plt.plot(np.sort(CWdata.rodOri.unique()), CW_post_probs[(np.where(frame_orients_all == 0)[0][0])], "bo",
+         label="CW data")
+plt.plot(np.sort(CCWdata.rodOri.unique()), CCW_post_probs[(np.where(frame_orients_all == 0)[0][0])], "r.",
+         label="CCW data")
+plt.plot(rod_orients_all, norm.cdf(rod_orients_all, CW_results.x[0], CW_results.x[1]), "-k", label="curve CW")
+plt.plot(rod_orients_all, norm.cdf(rod_orients_all, CCW_results.x[0], CCW_results.x[1]), "--c", label="curve CCW")
 plt.xlabel("Rod orientations in degrees")
 plt.ylabel("P(CW)")
 plt.title("For head = 0 and frame = 0")
+plt.legend()
 plt.show()
-
-
-
-
-
-
 
